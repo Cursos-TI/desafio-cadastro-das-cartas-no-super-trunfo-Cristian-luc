87b8f6a005c87bdba94b33b75be9391b68a99ac0
Cristian-luc <cristianbaixista@gmail.com>
2025-03-07 00:01:48 +0000

    primeros passos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,45 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char estado;               // Estado: letra de 'A' a 'H'
+    char codigo[5];           // Código da Carta: string (ex: A01, B03)
+    char nomeCidade[100];     // Nome da Cidade: string
+    int populacao;            // População: número de habitantes
+    float area;               // Área: em km²
+    float pib;                // PIB: Produto Interno Bruto
+    int pontosTuristicosc;     // Número de Pontos Turísticos
 
+    printf("Digite o estado (A-H): ");
+    scanf(" %c", &estado);
+    
+    printf("Digite o código da carta (ex: A01): ");
+    scanf("%s", &codigo);
+    
+    printf("Digite o nome da cidade: ");
+    scanf(" %c", &nomeCidade);
+    
+    printf("População: ");
+    scanf("%d" , &populacao);
+
+    printf("Pontos turisticos: ");
+    scanf("%f", &printf("Digite o estado (A-H): ");
+    scanf(" %c", &carta->estado);
+    
+    printf("Digite o código da carta (ex: A01): ");
+    scanf("%s", carta->codigo);
+    
+    printf("Digite o nome da cidade: ");
+    scanf(" %[^\n]", carta->nomeCidade); // Lê até a nova linha
+    
+    printf("Digite a população: ");
+    scanf("%d", &carta->populacao);
+    
+    printf("Digite a área (em km²): ");
+    scanf("%f", &carta->area);
+    
+    printf("Digite o PIB: ");
+    scanf("%f", &pib);
+    
+    
     return 0;
 }
