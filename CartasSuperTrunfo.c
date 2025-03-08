1a41d1ef3f0e35aae90119f353809a302ad7bc5a
Cristian-luc <cristianbaixista@gmail.com>
2025-03-08 01:34:19 +0000

    terminado2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,28 +12,38 @@ int main() {
     printf("Carta 1\n");
     printf("insira o código do estado 1: ");
     scanf("%s", estado1);
+    
     printf("digite o nome da cidade: ");
     scanf("%s", nomeCidade1);
+    
     printf("digite a população: ");
     scanf("%d", &populacao1);
+    
     printf("digite a área: ");
     scanf("%f", &area1);
+    
     printf("digite o PIB: ");
     scanf("%f", &pib1);
+    
     printf("digite a quantidade de pontos turisticos: ");
     scanf("%d", &pontosturisticos1);
 
     printf("Carta 2\n");
     printf("insira o código do estado 2: ");
     scanf("%s", estado2);
+    
     printf("digite o nome da cidade: ");
     scanf("%s", nomeCidade2);
+    
     printf("digite a população: ");
     scanf("%d", &populacao2);
+    
     printf("digite a área: ");
     scanf("%f", &area2);
+    
     printf("digite o PIB: ");
     scanf("%f", &pib2);
+    
     printf("digite a quantidade de pontos turisticos: ");
     scanf("%d", &pontosturisticos2);
 
