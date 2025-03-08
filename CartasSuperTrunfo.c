77138ff8fbb628d38e28b1fa83a48c89aa9a9687
Cristian-luc <cristianbaixista@gmail.com>
2025-03-08 01:05:33 +0000

    quase pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,4 @@
-#include 
+#include <stdio.h>
 
 int main() {
     char carta1[10], carta2[10];
@@ -11,24 +11,32 @@ int main() {
     printf("Carta 1\n");
     printf("insira o código do estado 1: ");
     scanf("%s", estado1);
+    
     printf("digite o nome da cidade: ");
     scanf("%s", nomeCidade1);
+    
     printf("digite a população: ");
     scanf("%d", &populacao1);
+    
     printf("digite a área: ");
     scanf("%f", &area1);
+    
     printf("digite o PIB: ");
     scanf("%f", &pib1);
-
+    
     printf("Carta 2\n");
     printf("insira o código do estado 2: ");
     scanf("%s", estado2);
+    
     printf("digite o nome da cidade: ");
     scanf("%s", nomeCidade2);
+    
     printf("digite a população: ");
     scanf("%d", &populacao2);
+    
     printf("digite a área: ");
     scanf("%f", &area2);
+    
     printf("digite o PIB: ");
     scanf("%f", &pib2);
 
