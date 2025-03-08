636458021c4ff082614006cc59ca2fd31a63942f
Cristian-luc <cristianbaixista@gmail.com>
2025-03-08 00:02:41 +0000

    ajuste2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
 
-int main() {
-    
-    char estado1, estado2;
-    char codigo1[5], codigo2[5];
-    char nomeCidade1[100], nomeCidade2[100];
-    int populacao1, populacao2;
-    float area1, area2;
-    float pib1, pib2;
+    int main() {
 
-    printf("Carta 1" \n);
-    printf("insira o código do estado 1: ");
-    scanf("%c", &estado1 \n);
+        char carta1[10], carta2[10];
+        char estado1, estado2;
+        char codigo1[5], codigo2[5];
+        char nomeCidade1[100], nomeCidade2[100];
+        int populacao1, populacao2;
+        float area1, area2;
+        float pib1, pib2;
+        
+        printf("Carta 1", &carta1);
+        printf("insira o código do estado 1: ");
+        scanf("%c", &estado1);
+        
+        printf("digite o nome da cidade: ");
+        scanf("%c", &nomeCidade1);
+        
+        printf("digite o a população: ");
+        scanf("%d", &populacao1);
+        
+        printf("digite a área:  ");
+        scanf("%f", &area1);
+        
+        printf("digite o PIB: ");
+        scanf("%f", &pib1);
+        
+        printf("Carta 2", &carta2);
+        printf("insira o código do estado 1: ");
+        scanf("%c", &estado2);
+        
+        printf("digite o nome da cidade: ");
+        scanf("%c", &nomeCidade2);
+        
+        printf("digite o a população: ");
+        scanf("%d", &populacao2);
+        
+        printf("digite a área:  ");
+        scanf("%f", &area2);
+        
+        printf("digite o PIB: ");
+        scanf("%f", &pib2)
 
-    printf("digite o nome da cidade: ");
-    scanf("%c", &nomeCidade1 \n);
-
-    printf("digite o a população: ");
-    scanf("%d", &populacao1 \n);
-
-    printf("digite a área:  ");
-    scanf("%f", &area1 \n);
-
-    printf("digite o PIB: ");
-    scanf("%f", &pib1 \n);
-
-    printf("Carta 2"\n);
-    printf("insira o código do estado 1: ");
-    scanf("%c", &estado2 \n);
-
-    printf("digite o nome da cidade: ");
-    scanf("%c", &nomeCidade2 \n);
-
-    printf("digite o a população: ");
-    scanf("%d", &populacao2 \n);
-
-    printf("digite a área:  ");
-    scanf("%f", &area2 \n);
-
-    printf("digite o PIB: ");
-    scanf("%f", &pib2 \n);
-    
-    return 0;
-}
+        return 0;
+    }
